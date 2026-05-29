6562cca4da2fdba3b738bfcc7e2e2ae139be6e98
flackko <philipsraphael2@gmail.com>
2026-05-29 21:04:52 +0100

    update project name, database config and environment variables

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Mongodb Connected");
   } catch (err) {
     console.error("Error connecting mongodb: ", err);
